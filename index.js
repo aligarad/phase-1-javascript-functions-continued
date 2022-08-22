c7c17f9ce58c17fb6298bffb9f4b39773a090f80
kenhunja <garadcali@gmail.com>
2022-08-22 14:44:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+ saturdayFun("bathe my dog");
+
+
+function mondayWork(activity = "go to the office"){
+   return `This Monday, I will ${activity}.` ;
+
+}
+   mondayWork("work from home");
+
+    
+
+  const wrapAdjective = (adj = "*") => {
+
+
+    return (clause = "a dedicated programmer") => 
+  {
+
+    return `You are ${adj}${clause}${adj}!`
+  }
+  }
